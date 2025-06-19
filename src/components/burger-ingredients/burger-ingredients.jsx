6573dcd4976487a74434f02b9dfea544fee7f235
Andrey Kruglov <kruglov.it@gmail.com>
2025-06-19 20:35:37 +0300

    initial commit

---
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -0,0 +1,50 @@
+import { Tab } from '@krgaa/react-developer-burger-ui-components';
+import PropTypes from 'prop-types';
+
+import { ingredientPropType } from '@utils/prop-types';
+
+import styles from './burger-ingredients.module.css';
+
+export const BurgerIngredients = ({ ingredients }) => {
+  console.log(ingredients);
+
+  return (
+    <section className={styles.burger_ingredients}>
+      <nav>
+        <ul className={styles.menu}>
+          <Tab
+            value="bun"
+            active={true}
+            onClick={() => {
+              /* TODO */
+            }}
+          >
+            Булки
+          </Tab>
+          <Tab
+            value="main"
+            active={false}
+            onClick={() => {
+              /* TODO */
+            }}
+          >
+            Начинки
+          </Tab>
+          <Tab
+            value="sauce"
+            active={false}
+            onClick={() => {
+              /* TODO */
+            }}
+          >
+            Соусы
+          </Tab>
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+BurgerIngredients.propTypes = {
+  ingredients: PropTypes.arrayOf(ingredientPropType.isRequired).isRequired,
+};
