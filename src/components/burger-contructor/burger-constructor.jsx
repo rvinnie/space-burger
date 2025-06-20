74519a026da39f9318f08d78ce93dbb3d0da9d51
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2025-06-20 20:22:57 +0400

    refactor((prop-types)): delete prop-types

--- src/components/burger-contructor/burger-constructor.jsx
+++ src/components/burger-contructor/burger-constructor.jsx
@@ -1,15 +1,7 @@
-import PropTypes from 'prop-types';
-
-import { ingredientPropType } from '@utils/prop-types';
-
 import styles from './burger-constructor.module.css';
 
 export const BurgerConstructor = ({ ingredients }) => {
   console.log(ingredients);
 
   return <section className={styles.burger_constructor}></section>;
-};
-
-BurgerConstructor.propTypes = {
-  ingredients: PropTypes.arrayOf(ingredientPropType.isRequired).isRequired,
 };
