75cae39bbc2adb4ff3d3b67e97271f8e27bd6bb1
Alexey Fedoseev <rv1nnie@yandex.ru>
2025-10-04 19:50:07 +0300

    feat: added react-router modal

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,10 @@
+import { Home } from '@/pages/home/home';
 import { Preloader } from '@krgaa/react-developer-burger-ui-components';
 import { useEffect } from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
 import { AppHeader } from '@components/app-header/app-header';
-import { BurgerConstructor } from '@components/burger-constructor/burger-constructor';
-import { BurgerIngredients } from '@components/burger-ingredients/burger-ingredients';
 import {
   getIngredients,
   getIngredientsLoading,
@@ -14,14 +12,20 @@ import {
   loadIngredients,
 } from '@services/burger-ingredients';
 
+import { IngredientDetailsWrapper } from '../burger-ingredients/ingredient-details-wrapper/ingredient-details-wrapper';
+import { IngredientDetails } from '../burger-ingredients/ingredient-details/ingredient-details';
+import { Modal } from '../modal/modal';
+
 import styles from './app.module.css';
 
 export const App = () => {
   const dispatch = useDispatch();
+  let location = useLocation();
 
   const ingredients = useSelector(getIngredients);
   const ingredientsLoading = useSelector(getIngredientsLoading);
   const ingredientsError = useSelector(getIngredientsError);
+  let state = location.state;
 
   useEffect(() => {
     dispatch(loadIngredients());
@@ -36,19 +40,38 @@ export const App = () => {
   return (
     <div className={styles.app}>
       <AppHeader />
-      <h1 className={`${styles.title} text text_type_main-large mt-10 mb-5 pl-5`}>
-        Соберите бургер
-      </h1>
       <main className={`${styles.main} pl-5 pr-5`}>
         {ingredientsLoading ? (
           <Preloader />
         ) : ingredientsError ? (
           errComponent
         ) : ingredients && ingredients.data && ingredients.data.length > 0 ? (
-          <DndProvider backend={HTML5Backend}>
-            <BurgerIngredients />
-            <BurgerConstructor />
-          </DndProvider>
+          <>
+            <Routes location={state?.backgroundLocation || location}>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/ingredients/:id"
+                element={
+                  <IngredientDetailsWrapper>
+                    <IngredientDetails />
+                  </IngredientDetailsWrapper>
+                }
+              />
+            </Routes>
+
+            {state?.backgroundLocation && (
+              <Routes>
+                <Route
+                  path="/ingredients/:id"
+                  element={
+                    <Modal header={'Детали ингредиента'}>
+                      <IngredientDetails />
+                    </Modal>
+                  }
+                />
+              </Routes>
+            )}
+          </>
         ) : (
           errComponent
         )}
